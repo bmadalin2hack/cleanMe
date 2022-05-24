d28f3ae5baae0ab9e2d13ad08b05253abc72acfe
Kant2wake <bmadalin94@gmail.com>
2022-05-24 21:31:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import Skills from "./components/Skills";
-import Work from "./components/Work";
-
+import Servicii from "./components/Servicii";
+import { Routes, Route } from "react-router-dom";
+import React from "react";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-      <Home />
-      <About />
-      <Skills />
-      <Work />
-      <Contact />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/servicii" element={<Servicii />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
